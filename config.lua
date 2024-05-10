ddefdffef1935d6832dd6b6b092662aa0882bde6
ZoniBoy00 <34036787+ZoniBoy00@users.noreply.github.com>
2024-05-10 15:14:06 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,26 +1,23 @@
+-- config.lua
 Projekti = {}
 
-Projekti.dispatch = "cd-dispatch" -- OPTIONS: 1 = "qs-dispatch",  2 = "cd-dispatch", 3 = "core-dispatch", 4 = "op-dispatch"
+Projekti.dispatch = "cd-dispatch" -- OPTIONS: 1 = "qs-dispatch", 2 = "cd-dispatch", 3 = "core-dispatch", 4 = "op-dispatch"
 
--- LOGS ARE IN s_projekti.lua 
+-- LOGS ARE IN s_projekti.lua
 
-Projekti.rob_only_once = true -- if set to FALSE then you can rob the same Parking Meter multiple times (no cooldowns etc)
+Projekti.rob_only_once = true -- if set to FALSE, you can rob the same Parking Meter multiple times (no cooldowns etc)
 
-Projekti.cooldown_time = 2 -- Cooldown is minutes Default = 2
+Projekti.cooldown_time = 2 -- Cooldown in minutes, Default = 2
 
-Projekti.progress_time = 25 -- default = 25 seconds
+Projekti.progress_time = 25 -- in seconds, default = 25
 
 Projekti.police_count = 1
-
-
 
 Projekti.notify = {
     ["text_color"] = "#ffffff",
     ["background_color"] = "#000000",
     ["icon"] = "fa-solid fa-tools",
-
 }
-
 
 Projekti.locales = {
     ["rob"] = "Rob Parking Meter",
